d5a53a325a9bb98537f9d32671db7de5adfd5626
Yurley Gallego <yurley.cetad@epfac.edu.co>
2021-09-30 21:55:08 -0500

    Se agrega componente Table, para la creación de tablas genericas

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Input from './components/common/Input'
+import PruebasTable from './components/pruebas/PruebasTable';
 
 function App() {
   return (
     <div className="App">
       <Input label='Nombre' type='text'></Input>
+      <PruebasTable></PruebasTable>
     </div>
   );
 }
---
+++ front/src/components/common/Table.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../../styles/Table.css';
+
+const Table = ({head, content}) => {
+    return (
+        <div>
+            <table>
+                <tr>
+                    {head.map(data => <th>{data.name}</th>)}
+                </tr>
+                {content.map(row => 
+                <tr>
+                    {head.map(data => <td>{row[data.value]}</td>)}
+               </tr>
+               )}
+            </table>
+        </div>
+    )
+}
+
+export default Table
