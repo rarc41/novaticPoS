3f6c242652dde9d704b815a0216720fc2114d7f3
Cristian <cristianespinosabarajas@gmail.com>
2021-10-03 16:30:08 -0500

    carga del login

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Input from './components/common/Input'
 import PruebasTable from './components/pruebas/PruebasTable';
+import Login from './components/login/Login'
 
 function App() {
   return (
     <div className="App">
-      <Input label='Nombre' type='text'></Input>
-      <PruebasTable></PruebasTable>
+      <Login />
+      {/* <Input label='Nombre' type='text'></Input>
+      <PruebasTable></PruebasTable> */}
     </div>
   );
 }
