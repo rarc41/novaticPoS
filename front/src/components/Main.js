abe464906c97830ff9b39a7de25ed68e2be477fe
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-10-04 20:16:29 -0500

    main layout done

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
-import Navbar from './common/Navbar';
+import React, { useState } from "react";
+import AppBar from "./common/AppBar";
+import Navbar from "./common/Navbar";
 
 const Main = () => {
+  const [section, setSection] = useState("vendedores");
+  return (
+    <main className="Main">
+      <Navbar />
 
-    const [section, setSection] = useState('vendedores')
-    return (
-        <main className="Main">
-            <Navbar/>
-        </main>
-    );
+      <AppBar></AppBar>
+    </main>
+  );
 };
 
 export default Main;
---
+++ front/src/components/common/AppBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/AppBar.css';
+
+
+const AppBar = () => {
+    return (
+        <div className="AppBar">
+            
+        </div>
+    );
+};
+
+export default AppBar;
