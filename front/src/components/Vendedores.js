b4cb4712ac95cda14c7c56f691eec15e810b2cab
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-10-05 23:30:20 -0500

    vendedores done!

--- front/src/components/Vendedores.js
+++ front/src/components/Vendedores.js
@@ -1,26 +1,46 @@
-import React, {useState} from 'react';
-import Button from './common/Button';
-import PruebasTable from './pruebas/PruebasTable';
-import PruebaModal from './common/Modal';
-import Search from './common/Search';
+import React, { useState } from "react";
+import Button from "./common/Button";
+import PruebasTable from "./pruebas/PruebasTable";
+import PruebaModal from "./common/Modal";
+import Search from "./common/Search";
 import Table from './common/Table';
+import ToolBar from "./common/ToolBar";
 
 const Vendedores = () => {
-
-  const[modalForm, setModalForm]= useState(false);
+  const [modalForm, setModalForm] = useState(false);
   const handleModalOpen = () => {
     setModalForm(!modalForm);
   };
 
+  const headers = [
+    { name: 'Nombre', value: 'name' },
+    { name: 'Especialidad', value: 'speciality' },
+    { name: 'Telefono', value: 'phone' },
+    { name: 'Fecha', value: 'date' },
+  ];
+  
+  const headers_test = ['nombre', 'especialidad', 'telefono', 'fecha']
+  
+  const values = [
+    { name: 'Alejandra', speciality: 'css', phone: '5677848', date: '16 de agosto'},
+    { name: 'Elias', speciality: 'react', phone: '7958540',  date: '16 de agosto' },
+    { name: 'Elias', speciality: 'react', phone: '7958540',  date: '16 de agosto' }
+  ];
+
   return (
-    <div  className="Module Module-container">
-        <Search label={"Usuario"}></Search>
+    <div className="Module Module-container">
+      {/* <Search label={"Usuario"}></Search>
       <Button label={'Nuevo Vendedor'} name={'btn-create'}
        icon={'fas fa-id-card'} onClick={handleModalOpen}></Button>
       <PruebasTable></PruebasTable>
       {modalForm && (
         <PruebaModal isOpen={modalForm} handleOpen={handleModalOpen}/>
-      )}
+      )} */}
+
+      <div className="Module Module-container product-section">
+        <ToolBar></ToolBar>
+        <Table headers={headers_test} data={values}></Table>
+      </div>
     </div>
   );
 };
