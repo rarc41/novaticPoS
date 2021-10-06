3454dd608537e9493e3fbc1c1e62e0775a065af0
Yurley Gallego <yurley.cetad@epfac.edu.co>
2021-10-05 19:23:11 -0500

    Se finaliza modulo de usuarios

--- front/src/components/Vendedores.js
+++ front/src/components/Vendedores.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react';
 import Button from './common/Button';
 import PruebasTable from './pruebas/PruebasTable';
 import PruebaModal from './common/Modal';
-import Modal from './common/Modal';
-import form from '../resources/json/prueba.json';
+import Search from './common/Search';
 
 const Vendedores = () => {
 
@@ -12,10 +11,9 @@ const Vendedores = () => {
     setModalForm(!modalForm);
   };
 
-
-
   return (
     <div  className="Module Module-container">
+        <Search label={"Usuario"}></Search>
       <Button label={'Nuevo Vendedor'} name={'btn-create'}
        icon={'fas fa-id-card'} onClick={handleModalOpen}></Button>
       <PruebasTable></PruebasTable>
@@ -25,8 +23,5 @@ const Vendedores = () => {
     </div>
   );
 };
-
-
-
 
 export default Vendedores;
