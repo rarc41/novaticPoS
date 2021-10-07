2524e96084cbff3e2d28f44205f5ec2c92bd4680
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-10-07 12:14:23 -0500

    ventas

--- front/src/components/Ventas.js
+++ front/src/components/Ventas.js
@@ -1,35 +1,77 @@
-import React, { useState } from 'react';
-import PruebaModal from './common/Modal';
-import PruebasVenta from './pruebas/PruebasVenta';
-import formulario from '../resources/json/venta.json';
-import { Button } from 'reactstrap';
-import '../styles/Button.css';
-import Search from './common/Search';
+import React, { useState } from "react";
+
+import "../styles/Button.css";
+import Table from './common/Table';
+import ToolBar from "./common/ToolBar";
 
 const Ventas = () => {
-  const [modalForm, setModalForm] = useState(false);
-  const handleModalOpen = () => {
-    setModalForm(!modalForm);
-  };
+  const headers = [
+    { name: "ID venta", value: "id" },
+    { name: "Valor venta", value: "value" },
+    { name: "Id producto", value: "product" },
+    { name: "Cantidad producto", value: "product_count" },
+    { name: "Precio producto", value: "product_price" },
+    { name: "fecha de venta", value: "date" },
+    { name: "ID cliente", value: "client_id" },
+    { name: "Nombre cliente", value: "client_name" },
+    { name: "Vendedor", value: "seller" },
+    { name: " ", value: "button" },
+  ];
+
+  const values = [
+    {
+      id: 123,
+      value: 10000,
+      product: 1,
+      product_count: 2,
+      product_price: 5000,
+      date: "2021-10-05",
+      client_id: 12345,
+      client_name: "Sara",
+      seller: "Alejandra",
+    },
+    {
+      id: 123,
+      value: 10000,
+      product: 1,
+      product_count: 2,
+      product_price: 5000,
+      date: "2021-10-05",
+      client_id: 12345,
+      client_name: "Sara",
+      seller: "Alejandra",
+    },
+    {
+      id: 123,
+      value: 10000,
+      product: 1,
+      product_count: 2,
+      product_price: 5000,
+      date: "2021-10-05",
+      client_id: 12345,
+      client_name: "Sara",
+      seller: "Alejandra",
+    },
+    {
+      id: 123,
+      value: 10000,
+      product: 1,
+      product_count: 2,
+      product_price: 5000,
+      date: "2021-10-05",
+      client_id: 12345,
+      client_name: "Sara",
+      seller: "Alejandra",
+    },
+  ];
+
+  const headersTest = Object.keys(values[0])
+
 
   return (
-    <div className="Module Module-container">
-      <div class="container">
-        <div class="row my-3">
-          <div class="col">{modalForm && <PruebaModal isOpen={modalForm} handleOpen={handleModalOpen} formulario={formulario} />}</div>
-          <div class="col">
-            <Search label={'Venta'}></Search>
-          </div>
-          <div class="col">
-            <Button className="buttonBox" color="btn btn-primary" onClick={handleModalOpen}>
-              Nueva Venta<i class="fas fa-plus-circle"></i>
-            </Button>
-          </div>
-        </div>
-        <div class="row">
-          <PruebasVenta></PruebasVenta>
-        </div>
-      </div>
+    <div className="Module Module-container product-section">
+      <ToolBar></ToolBar>
+      <Table headers={headersTest} data={values}></Table>
     </div>
   );
 };
