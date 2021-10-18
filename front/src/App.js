0357ff07944171983ea72d48348ea38403d4e668
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-10-17 21:12:37 -0500

    agregue axios, cliente axios y context para autenticacion

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Main from './components/Main';
-import Login from './components/login/Login';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Login from "./components/login/Login";
+import AuthState from "./context/autentication/authState";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/" component={Main}></Route>
-        <Route exact path="/login" component={Login}></Route>
-      </Switch>
-    </BrowserRouter>
+    <AuthState>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
+    </AuthState>
   );
 }
 
--- front/src/components/login/Login.js
+++ front/src/components/login/Login.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import './Login.css';
-import Logo from '../../resources/images/misiontic_logo.png';
-import Img1 from '../../resources/images/imagenLogin.png';
-import ImgGoogle from '../../resources/images/logoGoogle.png';
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import Logo from "../../resources/images/misiontic_logo.png";
+import Img1 from "../../resources/images/imagenLogin.png";
+import ImgGoogle from "../../resources/images/logoGoogle.png";
+import AuthContext from "../../context/autentication/authContext";
 
 function Login() {
+  const authContext = useContext(AuthContext);
+  const { registrarUsuario } = authContext;
+
+  const [login, setLogin] = useState({
+    usuario: "",
+    password: "",
+  });
+
+  const handleChange = e =>{
+    e.preventDefault();
+    setLogin({
+      ...login,
+      [e.target.name] : e.target.value
+    })
+
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    registrarUsuario({email:usuario, password})
+
+  }
+
+
+  const { usuario, password } = login;
+
+
   return (
     <div>
       <div className="logoMinTic">
         <img alt="logo" src={Logo} width="80" />
       </div>
       <div className="contenedor">
-        <form className="login">
+        <form className="login" onSubmit={handleSubmit}>
           <h1>LOGIN </h1>
           <label> Usuario o e-mail</label>
-          <input type="text" />
+          <input type="text" value={usuario} onChange={handleChange} name="usuario"/>
           <label> Contraseña </label>
-          <input type="password" className="inputPass" />
-          <button className="buttonContinuar">Continuar</button>
+          <input type="password" className="inputPass" value={password} name='password'onChange={handleChange}/>
+          <button className="buttonContinuar" type="submit">Continuar</button>
           <button className="buttonGoogle">
-            {' '}
+            {" "}
             <img alt="google" src={ImgGoogle} width="25" /> Continuar con Google
           </button>
         </form>
