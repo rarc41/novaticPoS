13a46ef8051bf10647c0c744bbda6b610748e19b
Cristian <cristianespinosabarajas@gmail.com>
2021-10-21 17:37:37 -0500

    cargo oauth con google

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Main from "./components/Main";
-import Login from "./components/login/Login";
-import AuthState from "./context/autentication/authState";
-=======
 import React, {useState} from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Main from './components/Main';
 import Login from './components/login/Login';
->>>>>>> Stashed changes
 
 function App() {
   const [loginIn, setLoginIn] = useState(false)
@@ -21,17 +13,6 @@ function App() {
     console.log("F")
   }
   return (
-<<<<<<< Updated upstream
-    <AuthState>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/login" component={Login}></Route>
-          <Route exact path="/" component={Main}></Route>
-          <Route exact path="/login" component={Login}></Route>
-        </Switch>
-      </BrowserRouter>
-    </AuthState>
-=======
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
@@ -41,7 +22,6 @@ function App() {
         <Route exact path="/" component={Login}></Route>
       </Switch>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 }
 
--- front/src/components/login/Login.js
+++ front/src/components/login/Login.js
@@ -1,40 +1,3 @@
-<<<<<<< Updated upstream
-import React, { useContext, useState } from "react";
-import "./Login.css";
-import Logo from "../../resources/images/misiontic_logo.png";
-import Img1 from "../../resources/images/imagenLogin.png";
-import ImgGoogle from "../../resources/images/logoGoogle.png";
-import AuthContext from "../../context/autentication/authContext";
-
-function Login() {
-  const authContext = useContext(AuthContext);
-  const { registrarUsuario } = authContext;
-
-  const [login, setLogin] = useState({
-    usuario: "",
-    password: "",
-  });
-
-  const handleChange = e =>{
-    e.preventDefault();
-    setLogin({
-      ...login,
-      [e.target.name] : e.target.value
-    })
-
-  }
-
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    registrarUsuario({email:usuario, password})
-
-  }
-
-
-  const { usuario, password } = login;
-
-
-=======
 import React from 'react';
 import './Login.css';
 import Logo from '../../resources/images/misiontic_logo.png';
@@ -49,26 +12,12 @@ import GoogleLogin from 'react-google-login';
 function Login({handleSuccess, handleFailure}) {
   console.log(handleSuccess)
 
->>>>>>> Stashed changes
   return (
     <div>
       <div className="logoMinTic">
         <img alt="logo" src={Logo} width="80" />
       </div>
       <div className="contenedor">
-<<<<<<< Updated upstream
-        <form className="login" onSubmit={handleSubmit}>
-          <h1>LOGIN </h1>
-          <label> Usuario o e-mail</label>
-          <input type="text" value={usuario} onChange={handleChange} name="usuario"/>
-          <label> Contraseña </label>
-          <input type="password" className="inputPass" value={password} name='password'onChange={handleChange}/>
-          <button className="buttonContinuar" type="submit">Continuar</button>
-          <button className="buttonGoogle">
-            {" "}
-            <img alt="google" src={ImgGoogle} width="25" /> Continuar con Google
-          </button>
-=======
         <form className="login">
           <h1>Log in to your account  </h1>
           <br /><br /><br /><br /><br /><br /><br /><br /><br />
@@ -90,7 +39,6 @@ function Login({handleSuccess, handleFailure}) {
                 )}
             />
           </div>
->>>>>>> Stashed changes
         </form>
         <div className="bienvenidos">
           <img alt="imagen" src={Img1} width="500" />
